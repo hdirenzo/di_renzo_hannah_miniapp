f3b28bf37b434684a7aada691a65dc1e6692005d
Hannah Di Renzo <hdirenzo@outlook.com>
2022-12-06 21:36:48 -0500

    fixing a few things

--- database/db_mini.sql
+++ database/db_mini.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 07, 2022 at 01:36 AM
+-- Generation Time: Dec 07, 2022 at 02:15 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -42,10 +42,10 @@ CREATE TABLE `tbl_cars` (
 --
 
 INSERT INTO `tbl_cars` (`cars_id`, `cars_name`, `cars_price`, `cars_engine`, `cars_trans`, `cars_speed`, `cars_img`) VALUES
-(1, '2023 Mini Convertiable', '$43,390 CAD', '3-Cylinder Twin Power Turbocharged Engine', '6-Speed Manual', '205km/h', 'imgs/img_cars_Convert.jpg'),
-(2, '2023 Mini All-Electric', '$45,590 CAD', 'Fully Electric Motor', 'Single-Speed Automatic', '150km/h', 'imgs/img_cars_Electric.jpg'),
-(3, '2023 Mini 5-Door', '$37,790 CAD', '3-Cylinder 12 Valve Twin Power Turbo', '6-Speed Manual ', '207km/h', 'imgs/img_cars_5Door.jpg'),
-(4, '2023 Mini 3-Door', '$36,790 CAD', '3-Cylinder 12 Valve Twin Power Turbo', '6-Speed Manual', '220km/h', 'imgs/img_cars_3Door.jpg');
+(1, '2023 Mini 3-Door', '$36,790 CAD', '3-Cylinder 12 Valve Twin Power Turbo', '6-Speed Manual', '220km/h', 'imgs/img_cars_3Door.jpg'),
+(2, '2023 Mini Convertiable', '$43,390 CAD', '3-Cylinder Twin Power Turbocharged Engine', '6-Speed Manual', '205km/h', 'imgs/img_cars_Convert.jpg'),
+(3, '2023 Mini All-Electric', '$45,590 CAD', 'Fully Electric Motor', 'Single-Speed Automatic', '150km/h', 'imgs/img_cars_Electric.jpg'),
+(4, '2023 Mini 5-Door', '$37,790 CAD', '3-Cylinder 12 Valve Twin Power Turbo', '6-Speed Manual ', '207km/h', 'imgs/img_cars_5Door.jpg');
 
 --
 -- Indexes for dumped tables
