1fa16498fdda1582efb9256f6c1a8597597e8fa0
Hannah Di Renzo <hdirenzo@outlook.com>
2022-12-07 20:26:39 -0500

    styling

--- includes/connect.php
+++ includes/connect.php
@@ -15,7 +15,7 @@ if (!$connection) {
 }
 
 $query_result = mysqli_query($connection, "
-SELECT cars_engine AS \"engine\", cars_price AS \"price\", tbl_models.models_name AS \"model\", tbl_modeltypes.modeltypes_name AS \"modelType\", cars_img AS \"imageUrl\" 
+SELECT cars_engine AS \"engine\", cars_price AS \"price\", tbl_models.models_name AS \"model\", tbl_modeltypes.modeltypes_name AS \"modelType\", cars_img AS \"imageUrl\" , cars_trans AS \"trans\", cars_speed AS \"speed\" 
 FROM tbl_cars 
 JOIN tbl_models ON tbl_models.models_id = tbl_cars.cars_model_id 
 JOIN tbl_modeltypes ON tbl_modeltypes.modeltypes_id = tbl_cars.cars_model_type_id;
